27ad2f4800702766b6e8406d975c5b2d3d2ae426
maks <maxim_86nva@mail.ru>
2023-05-20 14:48:40 +0300

    базовая часть

---
+++ src/com/hw/maxim/Employee.java
@@ -0,0 +1,55 @@
+package com.hw.maxim;
+
+public class Employee {
+
+    private int id;
+
+    private String fullName;
+
+    private int department;
+
+    private int salary;
+
+    private static int counter = 1;
+
+    public Employee(String fullName, int department, int salary) {
+        this.id = counter++;
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                '}';
+    }
+}
--- src/com/hw/maxim/Main.java
+++ src/com/hw/maxim/Main.java
@@ -3,6 +3,69 @@ package com.hw.maxim;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        Employee[] employees = new Employee[]{
+                new Employee("employee1", 1, 300000),
+                new Employee("employee2", 2, 231512),
+                new Employee("employee3", 3, 30232570),
+                new Employee("employee4", 4, 1234),
+                new Employee("employee5", 5, 3123400),
+                new Employee("employee6", 5, 3112300),
+                new Employee("employee7", 4, 63522),
+                new Employee("employee8", 3, 6543),
+                new Employee("employee9", 2, 23466),
+                new Employee("employee10", 1, 6244)
+        };
+
+        printAll(employees);
+        System.out.println("Суммарная зарлата: " + getTotalSalary(employees));
+        System.out.println("Сотрудник с минимальной зарлатой: " + getEmployeeWithMinSalary(employees));
+        System.out.println("Сотрудник с максимальной зарлатой: " + getEmployeeWithMaxSalary(employees));
+        System.out.println("Средняя зарлпата: " + getAvgSalary(employees));
+        printAllFullNames(employees);
+    }
+
+    public static void printAll(Employee[] employees) {
+        for (Employee e : employees) {
+            System.out.println(e);
+        }
+    }
+
+    public static int getTotalSalary(Employee[] employees) {
+        int result = 0;
+        for (Employee e : employees) {
+            result += e.getSalary();
+        }
+        return result;
+    }
+
+
+    public static Employee getEmployeeWithMinSalary(Employee[] employees) {
+        Employee employeeWithMinSalary = employees[0];
+        for (Employee e : employees) {
+            if (e.getSalary() < employeeWithMinSalary.getSalary()) {
+                employeeWithMinSalary = e;
+            }
+        }
+        return employeeWithMinSalary;
+    }
+
+    public static Employee getEmployeeWithMaxSalary(Employee[] employees) {
+        Employee employeeWithMaxSalary = employees[0];
+        for (Employee e : employees) {
+            if (e.getSalary() > employeeWithMaxSalary.getSalary()) {
+                employeeWithMaxSalary = e;
+            }
+        }
+        return employeeWithMaxSalary;
+    }
+
+    public static double getAvgSalary(Employee[] employees) {
+        return (double) getTotalSalary(employees) / employees.length;
+    }
+
+    public static void printAllFullNames(Employee[] employees) {
+        for (Employee e : employees) {
+            System.out.println(e.getFullName());
+        }
     }
 }
